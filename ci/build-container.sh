190097b7f67f8cd9691ef97bc1bcb84344b3775c
Piers Finlayson <piers@pierandkatie.com>
2019-03-12 19:41:01 +0000

    get libmbus working with scratch container

--- ci/build-container.sh
+++ ci/build-container.sh
@@ -15,15 +15,22 @@ fi
 if [[ $ARCH == "x86_64" ]];
   then
     TARGET="x86_64-unknown-linux-musl"
+    COMPILER_DIR="/opt/cross/x86_64/bin"
+    GCC="$COMPILER_DIR/x86_64-linux-musl-gcc"
 elif [[ $ARCH == "arm" ]]
   then
     TARGET="arm-unknown-linux-musleabihf"
+    COMPILER_DIR="/opt/cross/armv6/bin"
+    GCC="$COMPILER_DIR/arm-linux-musleabihf-gcc"
 elif [[ $ARCH == "armv7" ]]
   then
     TARGET="armv7-unknown-linux-musleabihf"
+    COMPILER_DIR="/opt/cross/armv7/bin"
+    GCC="$COMPILER_DIR/arm-linux-musleabihf-gcc"
 else
   output_args
 fi
+AR="$GCC-ar"
 
 TYPE=$2
 if [[ ! $TYPE ]];
@@ -60,7 +67,7 @@ fi
 
 DIR=tmp/$BIN-$TYPE-$ARCH-$VERSION
 CIDIR=ci/$DIR
-TAG=$BIN-$TYPE-$ARCH:$VERSION
+TAG=packom/$BIN-$TYPE-$ARCH:$VERSION
 
 echo "Creating container for"
 echo "  Binary:    $BIN"
@@ -69,6 +76,8 @@ echo "  Target:    $TARGET"
 echo "  Type:      $TYPE"
 echo "  Version:   $VERSION"
 echo "  Tag:       $TAG"
+echo "  gcc:       $GCC"
+echo "  ar:        $AR"
 
 rm -fr $DIDIR
 mkdir -p $CIDIR
@@ -77,6 +86,13 @@ echo "Getting API: ./api/openapi.yaml"
 cp ./api/openapi.yaml $CIDIR/api.yaml
 
 echo "docker build -t $TAG --build-arg DIR=$DIR --build-arg TYPE=$TYPE $NO_CACHE --build-arg TARGET=$TARGET ./ci"
-docker build -t $TAG --build-arg DIR=$DIR --build-arg TYPE=$TYPE $NO_CACHE --build-arg TARGET=$TARGET ./ci
+docker build -t $TAG \
+  --build-arg DIR=$DIR \
+  --build-arg TYPE=$TYPE \
+  --build-arg TARGET=$TARGET \
+  --build-arg GCC=$GCC \
+  --build-arg AR=$AR \
+  $NO_CACHE \
+  ./ci
 
 rm -fr $CIDIR
